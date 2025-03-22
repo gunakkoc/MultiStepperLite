ad343e3f81610e49e3a100be0b1eb31cacf65eb9
gunakkoc <gunakkoc@gmail.com>
2025-03-22 16:42:04 +0100

    Fixed license texts.

--- src/MultiStepperLite.h
+++ src/MultiStepperLite.h
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// https://github.com/gunakkoc/MultiStepperLite
 
 #ifndef MULTISTEPPERLITE_H
 #define MULTISTEPPERLITE_H
